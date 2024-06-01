b3fd71e6171bd18ce811bbd4ea9ea497ff926112
Yamini-toto <yamijoahi99@gmail.com>
2024-06-01 17:00:09 +0530

    third commit

---
+++ db.js
@@ -0,0 +1,23 @@
+// db.js
+require('dotenv').config();
+const mongoose = require('mongoose');
+console.log('MONGO_URI:', process.env.MONGO_URI);
+const connectDB = async () => {
+    try {
+        const uri = process.env.MONGO_URI;
+        if (!uri) {
+            throw new Error('MONGO_URI is not defined in the environment variables');
+        }
+
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected...');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
